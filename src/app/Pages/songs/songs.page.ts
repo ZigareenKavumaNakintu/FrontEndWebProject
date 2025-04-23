63eb47494ab281ff7247668c906929e06e8e9690
ZigareenKavumaNakintu <G00438436@atu.ie>
2025-04-23 15:58:43 +0000

    data binding in second page

--- src/app/Pages/songs/songs.page.ts
+++ src/app/Pages/songs/songs.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonInput, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.page.html',
   styleUrls: ['./songs.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonInput, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SongsPage implements OnInit {
-
+  title : string="";
+  artist :string="";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  OnSubmitClick(){
+
   }
 
 }
